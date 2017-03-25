4cc5269d1dd900724ce083a8fc8d662b2cfe268a
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2017-03-26 00:30:53 +0100

    Travis

--- hello.sh
+++ hello.sh
@@ -4,6 +4,13 @@ set -ev
 
 echo "hello.."
 
+
+# Install hugo
+wget https://github.com/spf13/hugo/releases/download/v0.19/hugo_0.19-64bit.deb
+dpkg -i hugo_0.19-64bit.deb
+
+
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 # https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
@@ -22,6 +29,16 @@ git config user.email "ci@scalatra.org"
 
 ls -al
 
+# Build scalatra site
+cd scalatra-website
+git checkout -b feature/hugo
+hugo -b https://takezoe.github.io/scalatra-website/ -d gh-pages
+
+ls -al
+ls -al gh-pages
+
+
+# Build scalatra apidocs
 cd scalatra
 
 sbt unidoc
