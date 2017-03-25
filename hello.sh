bed2ffe94979257805c7cf8f450fc8c5a3d80a81
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2017-03-25 23:52:20 +0100

    Travis

--- hello.sh
+++ hello.sh
@@ -4,6 +4,22 @@ set -ev
 
 echo "hello.."
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+git config user.name "Travis CI"
+git config user.email "ci@scalatra.org"
+
+
+
 ls -al
 
 cd scalatra
